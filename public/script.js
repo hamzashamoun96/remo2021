cb39b4829c7d4cc8bc84a94a6ef303ad8943a7df
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-04-20 21:32:45 +0300

    sgsdg

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
+  host:'https://remo2021.herokuapp.com',
   path: '/',
   port: '443',
   secure: true
